e0301e4ad623986259a72de497a1b872b6654fd8
unknown <none@ee.ee>
2020-09-11 11:14:39 +0300

    Заготовка проекта

--- src/sptvr19_gradusfahrenheit/App.java
+++ src/sptvr19_gradusfahrenheit/App.java
@@ -5,10 +5,20 @@
  */
 package sptvr19_gradusfahrenheit;
 
+import java.util.Scanner;
+
 /**
  *
  * @author user
  */
 class App {
-    
+    private Scanner scanner = new Scanner(System.in);
+    public void run() {
+        System.out.println("Конвентер градусов Цельсия в градусы по Фаренгейту");
+        System.out.println("Введите градусы по Цельсию: ");
+        double gradC = scanner.nextDouble();
+        System.out.println("По Фаренгейту это: ");
+        double gradF = gradC * 9 / 5 +32;
+        System.out.printf("%.2f%n", gradF);
+    }
 }
